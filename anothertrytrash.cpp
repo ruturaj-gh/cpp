7e33d0172c1de8b7c7600378563b23a809b1b16c
Ruturaj <rutughumkar33@gmail.com>
2022-05-08 12:41:23 +0530

    initial commit

--- anothertrytrash.cpp
+++ anothertrytrash.cpp
@@ -1,34 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int day_of_week(int y, int m, int d)
-{
-  static int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
-  y -= m < 3;
-  return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+static int fib(int n){
+  if(n==1 || n==2){
+    return 1;
+
+  }
+  else{
+    return fib(n-1)+fib(n-2);
+  }
+
+}
+
+static int anotherfib(int n,int memo[]){
+  int result=0;
+  if(memo[n]!='\0'){
+    return memo[n];
+  }
+  else if(n==1 || n==2){
+  result=1;
+    
+  }
+  else{
+    result=fib(n-1)+fib(n-2);
+    memo[n]=result;
+    return result;
+  }
+}
+static int feb_bottom_up(int n){
+  if(n==1 || n==2){
+    return 1;
+  }
+  int bottom_up[n]={};
+  bottom_up[0]=0;
+  bottom_up[1]=1;
+  for(int i=2;i<n;i++){
+    bottom_up[i]=bottom_up[i-1]+bottom_up[i-2];
+    
+  }
+  return bottom_up[n];
 }
 int main(){
-    int y,m,d;
-    cout<<"Enter the date(1-31): ";
-    cin>>d;
-    cout<<"Enter the month(1-12): ";
-    cin>>m;
-    cout<<"Enter the year(yyyy): ";
-    cin>>y;
-    switch(day_of_week(y,m,d)){
-        case 0: cout<<"\nSunday\n";
-        break;
-        case 1: cout<<"\nMonday\n";
-        break;
-        case 2: cout<<"\nTuesday\n";
-        break;
-        case 3: cout<<"\nWednesday\n";
-        break;
-        case 4: cout<<"\nThursday\n";
-        break;
-        case 5: cout<<"\nFriday\n";
-        break;
-        case 6: cout<<"\nSaturday\n";
-        break;
-    }
-    return 0;
+  int n=3;
+  int memo[n+1]={};
+  cout<<anotherfib(n,memo)<<endl;
+  cout<<fib(n)<<endl;
+  cout<<feb_bottom_up(n)<<endl;
+
+
+
+  return 0;
 }
--- trytrash4.cpp
+++ trytrash4.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool canJump(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<long long int> dp(n, 0.0);
+        dp[n-1] = 1;
+        for(int i = n - 2; i >= 0 ; i--)
+        {
+            int jumpVal = nums[i];
+            for(int j = 1; j <= jumpVal; j++)
+            {
+                if( i + j >= n)
+                    break;
+                dp[i] += dp[i+j];
+            }  
+        }
+        
+        for(int i = 0 ; i < n ; i++)
+            cout << dp[i] << " ";
+   
+        return dp[0] > 0;
+    }
 int main(){
-    vector<vector<int>>  arr{{1,2},{3,4},{5,6}};
-    cout<<arr[0][0];
-
-
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(11);
+    v.push_back(12);
+    v.push_back(13);
+    for(int i=0;i<v.size();i++){
+        cout<<v.at(i)<<endl;
+    }
+    cout<<canJump(v);
+    
+    
     return 0;
-}
+}    
+
